7a3dc98ca44794ea39b9976e37ac0caaae35f548
ABHINAV KUMAR <56204649+abhi9avx@users.noreply.github.com>
2025-05-12 10:44:07 +0530

    Add .gitignore and initial Main.java file
    - Created a .gitignore file to exclude IDE-specific files and build outputs for IntelliJ IDEA, Eclipse, NetBeans, and VS Code.
    - Added Main.java with a basic Java program that prints a welcome message and includes detailed comments on Java concepts, naming conventions, and data types.

---
+++ src/Main.java
@@ -0,0 +1,76 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+    }
+}
+ /*
+Explanation:
+	•	public – Access modifier (yeh method har jagah se accessible ho)
+	•	static – bina object banaye method call kar sakte hain
+	•	void – method kuch return nahi karta
+	•	main() – Java ka entry point hai
+	•	String[] args – command line se input lene ke liye hota hai
+
+Naming Conventions:
+	•	Class Name → PascalCase → MyFirstClass
+	•	Method Name → camelCase → runMyCode()
+	•	Constants → UPPER_CASE → MAX_SPEED
+
+Function -> A block of code that performs a task
+Method ->A function that is defined inside a class
+
+
+
+Recap:
+	•	Documentation → Comments likhne ke liye (optional but achha habit hai)
+	•	Package → Code ko organize karne ke liye (optional)
+	•	Import → Java ke dusre features ko lane ke liye (optional)
+	•	Interface → Advance OOP ke liye (optional)
+	•	Class → Har Java code ko class ke andar likhna zaroori hai
+	•	Main Method → Execution yahin se hota hai — yeh mandatory hai
+
+
+Data Type
+1. Primitive → Basic building blocks -> int , byte , long , double , float , double , char , bool , short
+2. Non-Primitive → User-defined or Java-defined complex types (String, Array, Object, etc.)-> it is derived from primittive data type
+
+
+int void  -> this variable is not possible ( kuch reserved work hai java mai which no one can use )
+
+Type: int
+Size: 4 bytes
+Example: 10, 200, -5
+
+Type: byte
+Size: 1 byte
+Example: -128 to 127
+
+Type: short
+Size: 2 bytes
+Example: 300, 1000
+
+Type: long
+Size: 8 bytes
+Example: 123456789L
+
+Type: float
+Size: 4 bytes
+Example: 3.14f
+
+Type: double
+Size: 8 bytes
+Example: 3.14159265
+
+Type: char
+Size: 2 bytes
+Example: 'A', 'z'
+
+Type: boolean
+Size: 1 bit
+Example: true, false
+
+
+*/
